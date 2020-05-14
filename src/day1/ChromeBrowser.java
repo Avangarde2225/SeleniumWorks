5174a589737a4ef7285bbc88da2d169eaa561993
Serdar <serdar2225@gmail.com>
2020-05-14 02:06:28 -0400

    selenium after a long time

--- src/day1/ChromeBrowser.java
+++ src/day1/ChromeBrowser.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeBrowser {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty( "webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get( "https://www.techno.study/" );
-
+        Thread.sleep(5000);
     }
 }
