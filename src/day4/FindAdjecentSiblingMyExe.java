4a6b1fe026533bc321cbbfaadd9cb4cfa03d32cc
Serdar <serdar2225@gmail.com>
2020-05-18 10:36:47 -0400

    adjecent exe

--- src/day4/FindAdjecentSiblingMyExe.java
+++ src/day4/FindAdjecentSiblingMyExe.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class FindAdjecentSibling {
+public class FindAdjecentSiblingMyExe {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty( "webdriver.chrome.driver", "C:\\Users\\suler\\Desktop\\Selenium\\chromedriver\\chromedriver.exe" );
         WebDriver driver = new ChromeDriver();
-        driver.get( "https://www.seleniumeasy.com/test/input-form-demo.html" );
+        driver.get( "http://automationpractice.com/index.php?controller=authentication&back=my-account" );
         WebElement firstElement = driver.findElement( By.cssSelector( ".form-group" ) );
 
         System.out.println(firstElement.getAttribute( "outerHTML" ));
         System.out.println("---------------------------------------");
-        WebElement siblingElement = driver.findElement( By.cssSelector( ".form-group + div" ) );
+        WebElement siblingElement = driver.findElement( By.cssSelector( ".form-group + p" ) );
 
         System.out.println(siblingElement.getAttribute( "outerHTML" ));
         driver.quit();
